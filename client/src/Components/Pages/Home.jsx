6072278dad78e8acdf334c018272e7303541ded0
Suman Karki <karkisuman0001@gmail.com>
2024-03-14 23:15:10 +0545

    comit

--- client/src/Components/Pages/Home.jsx
+++ client/src/Components/Pages/Home.jsx
@@ -6,15 +6,10 @@ import ChatArea from "./ChatArea";
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
-  const [fetchAgain, setFetchAgain] = useState(false);
-
-  const refreshSidebar = () => {
-    setFetchAgain(!fetchAgain);
-  };
 
   return (
     <div className="hm">
-      <SideBar fetchAgain={fetchAgain}></SideBar>
+      <SideBar></SideBar>
       <Outlet></Outlet>
       {/* <Welcome></Welcome> */}
       {/* <ChatArea props={chat[0]}></ChatArea> */}
